4eb7cc1902a5c9f4177daf9f1b947fb730eca310
goljavi <javiergold@live.com>
2024-02-12 15:00:59 -0300

    added recommendation function

---
+++ 3_recommendation_matrix.py
@@ -0,0 +1,16 @@
+import json
+from collections import defaultdict
+
+pairings_data = None
+with open('/formatted/ingredient-pairs.json', 'r', encoding='utf-8') as file:
+      pairings_data = json.loads(file.read())
+
+# Convert pairings data into a more accessible structure for recommendations
+# Create a dictionary to hold ingredient pairings and counts
+pairings = defaultdict(lambda: defaultdict(int))
+for pairing in pairings_data:
+    pairings[pairing["ing1"]][pairing["ing2"]] += pairing["count"]
+    pairings[pairing["ing2"]][pairing["ing1"]] += pairing["count"]
+
+with open('/formatted/ingredient-recomendation-matrix.json', 'w') as f:
+    json.dump(pairings, f, indent=4)
--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import pairings from './ingredient-recomendation-matrix.json';
 
 function App() {
+  React.useEffect(() => {    
+    // Recommendation function
+    function recommendIngredient(userIngredients) {
+        const recommendations = {};
+    
+        userIngredients.forEach(ingredient => {
+            if (pairings[ingredient]) {
+                Object.entries(pairings[ingredient]).forEach(([pair, count]) => {
+                    if (!userIngredients.includes(pair)) {
+                        recommendations[pair] = (recommendations[pair] || 0) + count;
+                    }
+                });
+            }
+        });
+
+        // Sort recommendations by count and return the top one
+        const recommended = Object.entries(recommendations).sort((a, b) => b[1] - a[1]).slice(0,3);
+        return recommended && recommended.length ? recommended.map(x => x[0]) : null;
+    }
+    
+    // Example user input
+    const userIngredients = ["carne", "ajo"];
+    // Get recommendation
+    const recommendation = recommendIngredient(userIngredients);
+    
+    console.log(recommendation);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
